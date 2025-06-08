16cfb15144a5bb13df671e1600102a413b4fb534
Tomás Cichero <martoxdlol@gmail.com>
2025-06-08 19:46:57 -0300

    trpc + nested async generators example

--- src/server/routers/root.ts
+++ src/server/routers/root.ts
@@ -4,6 +4,19 @@ export const appRouter = router({
     hello: publicProcedure.query(() => {
         return 'Hello from tRPC!'
     }),
+    prompt: publicProcedure.mutation(async function* () {
+        for (let i = 0; i < 30; i++) {
+            await new Promise((resolve) => setTimeout(resolve, 500));
+            yield numbersGen();
+        }
+    }),
 })
+
+const numbersGen = async function* () {
+    for (let i = 0; i < 30; i++) {
+        await new Promise((resolve) => setTimeout(resolve, 500));
+        yield i;
+    }
+}
 
 export type AppRouter = typeof appRouter
--- src/views/chat.tsx
+++ src/views/chat.tsx
@@ -1,10 +1,19 @@
+import { useMutation } from '@tanstack/react-query'
 import { useRef } from 'react'
 import { MessageInput } from '@/components/chat/input'
+import { Button } from '@/components/ui/button'
 import { useElementDimensions } from '@/hooks/use-element-dimensions'
+import { useTRPC } from '@/lib/api-client'
 
 export function ChatView() {
     const inputWrapperRef = useRef<HTMLDivElement>(null)
     const inputWrapperSize = useElementDimensions(inputWrapperRef.current)
+
+    const trpc = useTRPC()
+
+    const mutation = useMutation(trpc.prompt.mutationOptions())
+
+    console.log(mutation.data)
 
     return (
         <div
@@ -12,10 +21,27 @@ export function ChatView() {
             style={{ paddingBottom: `${inputWrapperSize.height}px` }}
         >
             <div className='w-full max-w-3xl'>Messages</div>
+            <Button
+                onClick={() => {
+                    mutation.mutateAsync().then(async (data) => {
+                        for await (const item of data) {
+                            logGenerator(item)
+                        }
+                    })
+                }}
+            >
+                Start Prompt
+            </Button>
 
             <div className='fixed bottom-0 w-full max-w-3xl' ref={inputWrapperRef}>
                 <MessageInput />
             </div>
         </div>
     )
+}
+
+async function logGenerator(data: AsyncIterable<number>) {
+    for await (const item of data) {
+        console.log(item);
+    }
 }
