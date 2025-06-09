0a1ce9e2f2f490f4c8263d861173776586ec35b8
Tomás Cichero <martoxdlol@gmail.com>
2025-06-09 17:08:28 -0300

    fix back button mobile

--- src/components/chat/appbar.tsx
+++ src/components/chat/appbar.tsx
@@ -1,10 +1,9 @@
 import { useQuery } from '@tanstack/react-query'
 import { ChevronLeftIcon } from 'lucide-react'
 import { useMemo } from 'react'
-import { Link, useLocation, useParams } from 'react-router'
+import { useLocation, useNavigate, useParams } from 'react-router'
 import { useTRPC } from '@/lib/api-client'
 import { IconButton } from '../icon-button'
-import { Button } from '../ui/button'
 
 export function ChatAppBar() {
     const location = useLocation()
@@ -25,21 +24,21 @@ export function ChatAppBar() {
 
     const canGoBack = location.state === 'back-to-home'
 
+    const navigate = useNavigate()
+
     return (
         <div className='flex h-14 items-center gap-2 px-4'>
-            {chatId && (
-                <>
-                    {canGoBack && <IconButton onClick={() => window.history.back()} icon={<ChevronLeftIcon />} aria-label='back' />}
-                    {!canGoBack && (
-                        <Button asChild variant='ghost' className=''>
-                            <Link to='/chats'>
-                                <ChevronLeftIcon className='md:hidden' />
-                            </Link>
-                        </Button>
-                    )}
-                </>
+            {canGoBack && (
+                <IconButton onClick={() => window.history.back()} icon={<ChevronLeftIcon />} aria-label='back' />
             )}
-
+            {!canGoBack && (
+                <IconButton
+                    onClick={() => navigate('/chats')}
+                    className='md:hidden'
+                    icon={<ChevronLeftIcon />}
+                    aria-label='back'
+                />
+            )}
             <h2 className=''>{chatTitle}</h2>
         </div>
     )
