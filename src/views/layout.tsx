1740d6835cc2e3a5dc5e55e7b4dbcb1ae13d1245
Tomás Cichero <martoxdlol@gmail.com>
2025-06-08 23:02:31 -0300

    hide menu on mobile

--- src/views/layout.tsx
+++ src/views/layout.tsx
@@ -3,7 +3,7 @@ import { Sidenav } from "@/components/nav";
 export function Layout(props: { children: React.ReactNode }) {
     return (
         <div className='flex size-full'>
-            <nav className='w-64 shrink-0 overflow-y-auto'>
+            <nav className='hidden w-64 shrink-0 overflow-y-auto sm:block'>
                 <Sidenav />
             </nav>
             <main className='flex min-w-0 shrink grow justify-center'>{props.children}</main>
