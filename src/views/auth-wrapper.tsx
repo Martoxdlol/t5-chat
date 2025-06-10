793cf3a35bc0c50a01601d4fba64cbc11c5f03ee
Tomás Cichero <martoxdlol@gmail.com>
2025-06-10 12:45:06 -0300

    login screen

--- src/views/auth-wrapper.tsx
+++ src/views/auth-wrapper.tsx
@@ -1,4 +1,5 @@
-import { Layout } from '@/components/layout'
+import { Center } from '@/components/center'
+import { GoogleSvg } from '@/components/google'
 import { Button } from '@/components/ui/button'
 import { useAuth } from '@/hooks/use-auth'
 import { authClient } from '@/lib/auth-client'
@@ -10,9 +11,15 @@ export function AuthWrapper(props: { children: React.ReactNode }) {
 
     if (!user) {
         return (
-            <Layout sidenav={null} hasContent>
-                <Button onClick={() => authClient.signIn.social({ provider: 'google' })}>Sign in with Google</Button>
-            </Layout>
+            <Center>
+                <div className='flex flex-col items-center gap-4'>
+                    <h1 className='text-3xl'>T5 Chat</h1>
+                    <Button className='bg-white' onClick={() => authClient.signIn.social({ provider: 'google' })}>
+                        <GoogleSvg />
+                        <span className='mx-2'>Sign in with Google</span>
+                    </Button>
+                </div>
+            </Center>
         )
     }
 
